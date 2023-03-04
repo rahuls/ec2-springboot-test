ad1a20b2434a23dcd6b4b0935f566179746bfc1b
Rahul Sudhanaboina <sudhanaboina.rahul@gmail.com>
2023-03-04 14:52:45 +0000

    Intial commit

---
+++ src/main/java/com/test/Ec2SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2SpringbootApplication.class, args);
+	}
+
+}
